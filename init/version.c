f7a89fa9fc48135b84dbf25d1d59e0a5117fbfaa
JohnFrancisMcFall <lucascamargos478@gmail.com>
2023-02-15 03:44:25 +0530

    Fix build again

--- init/version.c
+++ init/version.c
@@ -42,9 +42,9 @@ EXPORT_SYMBOL_GPL(init_uts_ns);
 
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
-	"Linux version " UTS_RELEASE " ("JohnFrancisMcFall") (" LINUX_COMPILER ") " UTS_VERSION "\n";
+	"Linux version " UTS_RELEASE " (JohnFrancisMcFall) (" LINUX_COMPILER ") " UTS_VERSION "\n";
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" ("JohnFrancisMcFall")"
+	" (JohnFrancisMcFall)"
 	" (" LINUX_COMPILER ") %s\n";
